049d03aa2c49333e1dc9312a11c55f4ae011ad7b
ssingh954 <112465470+ssingh954@users.noreply.github.com>
2022-09-06 17:00:22 +0530

    Update matchstick.js

--- matchstick.js
+++ matchstick.js
@@ -1,7 +1,7 @@
 function matchHouses(step) {
-    return step === 0 ? 0 : step * 4 + (step + 1);
-  }
+  return step === 0 ? 0 : step * 4 + (step + 1);
+}
 
-  console.log(matchHouses(1));
-  console.log(matchHouses(4));
-  console.log(matchHouses(87));
+console.log(matchHouses(1));
+console.log(matchHouses(4));
+console.log(matchHouses(87));
